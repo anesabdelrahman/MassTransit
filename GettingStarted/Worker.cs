dae290033f801f607b57f9fdc97fadf74f8128fc
Anes Abdelrahman <anes.abdelrahman@gmail.com>
2025-12-28 11:50:06 +0000

    Added background service to publish messages

---
+++ GettingStarted/Worker.cs
@@ -0,0 +1,27 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Contracts;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+
+namespace GettingStarted;
+
+public class Worker : BackgroundService
+{
+    readonly IBus _bus;
+
+    public Worker(IBus bus)
+    {
+        _bus = bus;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _bus.Publish(new HelloMessage{Name = "Anes World!"}, stoppingToken);
+        }
+        
+        await Task.Delay(10000000, stoppingToken);
+    }
+}
